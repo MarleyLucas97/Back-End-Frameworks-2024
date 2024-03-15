75028a672de68b0823be35c6279f947798ab3343
Marley De Lucas <marleydelucas97@gmail.com>
2024-03-15 20:37:54 -0300

    back to back 03.15.24

--- Trabalho Pratico Back End 01/Pousada Ecologica Do Brasilia/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("botao-menu").addEventListener("click", function(){
-    var navbar = document.getElementById("navbar-secundaria");
-    if (navbar.style.display === "none"){
-        navbar.style.display = "block";
-    }else{
-        navbar.style.display = "none";
-    }
-});
